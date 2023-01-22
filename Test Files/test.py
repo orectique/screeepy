4a66dacc18be140847dcfc31bd1bb761252e1a7e
Sreehari Pulickamadhom Sreedhar <orectique@gmail.com>
2023-01-22 15:49:55 +0530

    done

---
+++ Test Files/test.py
@@ -0,0 +1,16 @@
+a = 5
+b = 6
+
+c = 9
+#this is a comment
+
+  # this is a markdown cell
+
+def fun(a, b):
+    if a == 5:
+        return a + b
+
+    return a - b
+
+print(sum(a, b))
+print(c**2)
